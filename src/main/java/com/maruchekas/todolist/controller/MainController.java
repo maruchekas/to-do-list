79f955d00f88e2a33453b086a493738061674d30
maruchekas <salken84@gmail.com>
2021-11-11 13:52:14 +0300

    added a to-do list to the main page

--- src/main/java/com/maruchekas/todolist/controller/MainController.java
+++ src/main/java/com/maruchekas/todolist/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.maruchekas.todolist.controller;
 
+import com.maruchekas.todolist.model.Task;
+import com.maruchekas.todolist.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+  @Autowired
+  private TaskRepository taskRepository;
+
   @GetMapping("/")
   public String index(Model model){
     model.addAttribute("title", "Список дел");
+    Iterable<Task> tasks = taskRepository.findAll();
+    model.addAttribute("tasks", tasks);
     return "index";
   }
 
